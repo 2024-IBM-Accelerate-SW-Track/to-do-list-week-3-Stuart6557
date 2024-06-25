c069e76748db24ff8982ad238acc67131c66bda9
Stuart6557 <vivianliu532@gmail.com>
2024-06-24 20:58:20 -0700

    finished lab 3

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -19,25 +19,92 @@ afterEach(() => {
 
 
 
- test('test that App component doesn\'t render dupicate Task', () => {
+test('test that App component doesn\'t render dupicate Task', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "06/24/2024";
+  // Add task for the first time
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  // Check if task was added
+  const check = screen.getByText(/History Test/i);
+  const checkDate = screen.getByText(new RegExp(dueDate, "i"));
+  expect(check).toBeInTheDocument();
+  expect(checkDate).toBeInTheDocument();
+  // Add task for the second time
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  // Check if task was added only once
+  check = screen.getByText(/History Test/i);
+  const tasks = screen.getAllByText(/History Test/i);
+  expect(tasks.length).toBe(1);
+});
 
- test('test that App component doesn\'t add a task without task name', () => {
+test('test that App component doesn\'t add a task without task name', () => {
   render(<App />);
- });
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "06/24/2024";
+  // Attempt to add task
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  // Check that task was not added
+  const check = screen.getByText(/History Test/i);
+  const checkDate = screen.getByText(new RegExp(dueDate, "i"));
+  expect(check).not.toBeInTheDocument();
+  expect(checkDate).not.toBeInTheDocument();
+});
 
- test('test that App component doesn\'t add a task without due date', () => {
+test('test that App component doesn\'t add a task without due date', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "06/24/2024";
+  // Attempt to add task
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.click(element);
+  // Check that task was not added
+  const check = screen.getByTestId(/History Test/i);
+  const checkDate = screen.getByText(new RegExp(dueDate, "i"));
+  expect(check).not.toBeInTheDocument();
+  expect(checkDate).not.toBeInTheDocument();
+});
 
 
 
- test('test that App component can be deleted thru checkbox', () => {
+test('test that App component can be deleted thru checkbox', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "06/24/2024";
+  // Add task
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  // Find checkbox and click it to delete the task
+  const checkbox = screen.getByRole('checkbox', { name: /History Test/i });
+  fireEvent.click(checkbox);
+  // Check that task was deleted
+  expect(task).not.toBeInTheDocument();
+});
 
 
- test('test that App component renders different colors for past due events', () => {
+test('test that App component renders different colors for past due events', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "01/01/2020";
+  // Add task
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  // Check if task was added with the correct color
+  const check = screen.getByTestId(/History Test/i).style.background
+  expect(check).toBe("pink");
+});
